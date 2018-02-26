a3bad2fe5fb957925ba322d086e363775aa236c5
Aliaksei Babko <alexeibobko@gmail.com>
2018-02-26 18:40:35 +0300

    solution with comparator

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ class Sorter {
 
   add(element) {
     // your implementation
-    //return this.array.push({"age"});
+    //return this.array.push({"age"});     
     return this.array.push(element);
   }
 
@@ -32,24 +32,31 @@ class Sorter {
   sort(indices) {
     // your implementation
     let i, j;
-    if (indices.length > 1) {
-      indices.sort();
-      for (j=0; j<=(indices.length-1); j++) {
-        for (i=0; i<indices.length; i++) {
-          if (this.array[indices[i]] > this.array[indices[i+1]]) {
-            var a = this.array[indices[i]];
-            this.array[indices[i]] = this.array[indices[i+1]];
-            this.array[indices[i+1]] = a;
+    if (!(this.comparator)) {
+      if (indices.length > 1) {
+        indices.sort();
+        for (j=0; j<=(indices.length-1); j++) {
+          for (i=0; i<indices.length; i++) {
+            if (this.array[indices[i]] > this.array[indices[i+1]]) {
+              var a = this.array[indices[i]];
+              this.array[indices[i]] = this.array[indices[i+1]];
+              this.array[indices[i+1]] = a;
+            }
           }
         }
       }
+      return this.array;
     }
-  return this.array;
-}
+    else {
+      this.array.sort(this.comparator(indices[0], indices[1]))
+      return this.array;
+    }
+  }
 
   setComparator(compareFunction) {
     // your implementation
-  AGE_COMPARATOR
+    this.comparator = compareFunction;
+  return this.comparator;
   }
 }
 
